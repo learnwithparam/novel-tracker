dbdc503d0f3db4bd8ee0d06d5c7e967838adb4b1
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2020-04-13 22:25:30 +0300

    Fix tailwind theme not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darken-blue-100': '#303753',
+        'darken-blue-200': '#212A46'
+      },
+    },
   },
   variants: {},
   plugins: [],
