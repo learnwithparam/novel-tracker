95b24b17675b7caf8ed10af5513240a4ca89df42
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2020-04-18 16:23:57 +0300

    Extend box shadow for mobile menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
         'darken-blue-100': '#303753',
         'darken-blue-200': '#212A46'
       },
+      boxShadow: {
+        'top-xl': '0px -4px 4px rgba(0, 0, 0, 0.25)'
+      }
     },
   },
   variants: {},
