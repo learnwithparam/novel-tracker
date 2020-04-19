ee99fe94826887a1273a096aaba1cdd8709949b2
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2020-04-19 15:48:17 +0300

    Setup base theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'darken-blue-100': '#303753',
-        'darken-blue-200': '#212A46'
+        primary: 'var(--text-primary)',
+        secondary: 'var(--text-secondary)',
+        background: {
+          primary: 'var(--bg-background-primary)',
+          secondary: 'var(--bg-background-secondary)',
+          tertiary: 'var(--bg-background-tertiary)',
+        },
       },
       boxShadow: {
         'top-xl': '0px -4px 4px rgba(0, 0, 0, 0.25)'
